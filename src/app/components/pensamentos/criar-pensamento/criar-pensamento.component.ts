63763d0b5e3a3793d9ca897e7638331130b983fd
F9519992 Victor Hugo Vieira De Oliveira <victorhugo.vieira@bb.com.br>
2025-05-27 17:15:05 -0300

    aula 2 parte 6/9

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: '',
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  criarPensamento() {
+    alert('Novo pensamento criado! ');
   }
 
+  cancelarPensamento() {
+    this.pensamento.conteudo = '';
+    this.pensamento.autoria = '';
+    alert('Pensamento cancelado. ');
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
